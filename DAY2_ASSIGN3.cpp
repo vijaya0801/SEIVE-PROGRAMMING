7862a6ae209a97184d3250ebf62b7ff8b23e4963
vijaya0801 <67973599+vijaya0801@users.noreply.github.com>
2021-05-14 19:55:07 +0530

    Add files via upload

---
+++ DAY2_ASSIGN3.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+long long int N=1000001;
+long long int seive[1000001];
+void gen_modifiedsieve()
+{
+    seive[0]=seive[1]=1;
+    for(int i=2;i<=N;i++)seive[i]=i;
+    for(int i=2;i*i<=N;i++)
+    {
+        if(seive[i]==i)
+        {
+            for(int j=i*i;j<=N;j+=i)
+            {
+                if(seive[j]==j)
+                {
+                    seive[j]=i;
+                }
+            }
+        }
+    }
+    
+}
+
+int main() {
+	gen_modifiedsieve();
+	int q;
+	cin>>q;
+	while(q--)
+	{
+	    int n,max=0;
+	    cin>>n;
+	    while(n!=1)
+	    {
+	    	//cout<<seive[n]<<" ";
+	        n=n/seive[n];
+	        if(max<seive[n]) max=seive[n];
+	    }
+	    cout<<"\n";
+	    cout<<max;
+	}
+	
+	return 0;
+}
